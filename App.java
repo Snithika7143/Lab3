6b427353be3283aa78bdc94dc6e203ca5e3bfef0
Snithika <SnithikaReddy.Gaddam1@marist.edu>
2025-02-14 17:53:16 -0500

    Updated greeting message (Fixes #1)

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub Actions! This is [Your Name].");
+        System.out.println("Hello, GitHub Actions! This is My modified name Snithika Reddy Gaddam.");
     }
 }
